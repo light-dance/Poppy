6be0893272b263f3249434851090a35182cc0e00
Jake Go <jake@notnotjake.com>
2026-06-04 15:06:56 -0400

    migrations

--- web/db/migrations/20260604140000_add_sparkle_release_metadata/migration.sql
+++ web/db/migrations/20260604140000_add_sparkle_release_metadata/migration.sql
@@ -3,5 +3,3 @@ ALTER TABLE `releases` ADD `build` integer DEFAULT 1 NOT NULL;
 ALTER TABLE `releases` ADD `sparkle_zip_length` integer;
 --> statement-breakpoint
 ALTER TABLE `releases` ADD `sparkle_zip_signature` text;
---> statement-breakpoint
-CREATE UNIQUE INDEX `releases_version_build_idx` ON `releases` (`version`,`build`);
