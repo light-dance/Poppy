961bd26b5c75c5ddef2f283839b266cb9cf00675
Jake Go <jake@notnotjake.com>
2026-06-03 22:58:30 -0400

    fresh migrations

--- web/db/migrations/20260604025711_curved_eternity/migration.sql
+++ web/db/migrations/20260604025711_curved_eternity/migration.sql
@@ -1,11 +1,10 @@
 CREATE TABLE `releases` (
-	`build_number` integer PRIMARY KEY,
-	`version` text NOT NULL,
+	`version` text PRIMARY KEY NOT NULL,
+	`title` text,
 	`changelog` text DEFAULT '' NOT NULL,
 	`published_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`updated_at` integer DEFAULT (unixepoch()) NOT NULL
 );
 --> statement-breakpoint
-CREATE INDEX `releases_version_idx` ON `releases` (`version`);--> statement-breakpoint
 CREATE INDEX `releases_published_at_idx` ON `releases` (`published_at`);
